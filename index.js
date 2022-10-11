5353fd6441f92146502179ff552dbfbb53b9bcbb
Glitched-Json <40910947+Glitched-Json@users.noreply.github.com>
2022-10-11 11:41:41 +0300

    Calculate Function Implemented

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//https://discord.com/api/oauth2/authorize?client_id=1028820655333986304&permissions=67584&scope=bot%20applications.commands
+
 const Discord = require("discord.js")
 require("dotenv").config()
 const client = new Discord.Client({
@@ -17,20 +19,22 @@ client.on("messageCreate", (message) => {
     if (!message.guild) return
     if (message.author.bot) return
     
-    if (message.content.startsWith(prefix)) {
+    if (message.content.startsWith("Dicem change prefix:")) {
+        message.reply("Dicem prefix changed to: "+(prefix = message.content.substring(20).trim()))
+    } else if (message.content == "Dicem get prefix") {
+        message.reply("Dicem prefix is currently: "+prefix)
+    } else if (message.content == "Dicem reset prefix") {
+        message.reply("Dicem prefix reverted back to: "+(prefix = '!r'))
+    } else if (message.content.startsWith(prefix)) {
         const msg = message.content.slice(prefix.length).trim()
-        message.reply(calculate(msg).toString())
-    } else if (message.content.startsWith("Dicem change prefix:")) {
-        prefix = message.content.substring(20)
+        message.reply(calculate(msg))
     }
 })
 
 function calculate(msg) {
     ooo = [
         [
-            "d"
-        ],
-        [
+            "d",
             "kh",
             "kl",
             "min",
@@ -46,8 +50,9 @@ function calculate(msg) {
         ]
     ]
     tokens = msg.split(/([+-/*d]|kh|kl|min|max)/g)
-
-    for (i = 0; i<ooo.length; i++) {
+    message = Array.from({length: tokens.toString().match(/d/g)}, (v, i) => null)
+    
+    for (index = -1, i = 0; i<ooo.length; i++) {
         for (j = 0; j<tokens.length; j++) {
             if (ooo[i].includes(tokens[j])) {
                 a = tokens[j-1]
@@ -67,49 +72,65 @@ function calculate(msg) {
                         break;
                     case "kh":
                         arr = a.split(',').map(x => parseInt(x)).sort(function(a, b){return b-a})
-                        arr.splice(parse(b, 1))
+                        reduced = arr.splice(parse(b, 1))
                         r = arr.toString()
+                        message[index] = message[index].map(x => {if (reduced.map(x => x.toString()).includes(x)) return '~~'+x+'~~'; else return x;})
                         break;
                     case "kl":
                         arr = a.split(',').map(x => parseInt(x)).sort(function(a, b){return a-b})
-                        arr.splice(parse(b, 1))
+                        reduced = arr.splice(parse(b, 1))
                         r = arr.toString()
+                        message[index] = message[index].map(x => {if (reduced.map(x => x.toString()).includes(x)) return '~~'+x+'~~'; else return x;})
                         break;
                     case "min":
-                        if (b == '' || isNaN(parse(b)))
+                        if (b == '' || isNaN(parse(b))) {
                             r = a.split(',').map(x => parseInt(x)).sort(function(a, b){return a-b})[0].toString()
-                        else
-                            r = Math.max(parse(a), parse(b)).toString()
-                        break;
+                            message[index] = message[index].map(x => x.replaceAll('~','')).map(x => {if (x!=r) return "~~"+x+"~~"; else return x})
+                        } else {
+                            m = Math.max(parse(a), parse(b))
+                            r = m.toString()
+                            if (m = parse(b)) message[index] = "~~"+message[index].map(x => x.replaceAll('~', '')).toString().replaceAll(',',', ')+"~~"
+                        } break;
                     case "max":
-                        if (b == '' || isNaN(parse(b))) 
+                        if (b == '' || isNaN(parse(b))) {
                             r = a.split(',').map(x => parseInt(x)).sort(function(a, b){return b-a})[0].toString()
-                        else
-                            r = Math.min(parse(a), parse(b)).toString()
-                        break;
+                            message[index] = message[index].map(x => x.replaceAll('~','')).map(x => {if (x!=r) return "~~"+x+"~~"; else return x})
+                        } else {
+                            m = Math.min(parse(a), parse(b))
+                            r = m.toString()
+                            if (m = parse(b)) message[index] = "~~"+message[index].map(x => x.replaceAll('~', '')).toString().replaceAll(',',', ')+"~~"
+                        } break;
                     case "d":
                         r = Array.from({length: parse(a, 1)}, (v, i) => Math.floor(Math.random()*parse(b, 20)+1)).toString()
+                        message[++index] = r.split(',')
                         break;
                 }
                 tokens.splice(j+1, 1)
                 tokens.splice(j, 1, r)
                 tokens.splice(j-1, 1)
                 j=Math.max(j-1, 0)
-                console.info(tokens)
             }
         }
     }
 
     function parse(str, def = 0) {
         result = 0
-        if (str.includes(',')) str.split(',').forEach(x => {result += parseInt(x)})
+        if (str.includes(',')) str.split(',').forEach(x => result += parseInt(x))
         else result = parseInt(str)
 
         if (isNaN(result)) return def
         else return result
     }
 
-    return parse(tokens)
+    function regexIndexOf(text, reg, i) {
+    var indexInSuffix = text.slice(i).search(reg);
+    return indexInSuffix < 0 ? indexInSuffix : indexInSuffix + i;
+}
+    
+    temp = msg; format = ""; res = temp.match(/([0-9]*d[^+-\/*]*)/g);
+    message.forEach((x, i) => {format += temp.substring(0, temp.search(res[i])+res[i].length)+"("+x.toString().replaceAll(",",", ")+")"; temp = temp.slice(temp.search(res[i])+res[i].length)})
+    format += temp
+    return format + "\nTotal: " + parse(tokens.toString()).toString()
 }
 
 client.login(process.env.TOKEN)
